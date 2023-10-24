a7bdcf636d7f062ca978bebc5cd71cf4377cdd8b
eli4sMD <eliasdomin2003@gmail.com>
2023-10-24 12:42:09 -0300

    algunas mejoras

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -8,15 +8,22 @@ function calculateDensity(pressure, depth) {
     let pressureInPascals = pressure * 6894.76; // convertir la presión de psia a Pascales
     let density = pressureInPascals / (g * depth); // calcular la densidad en kg/m³
     return density;
-  }
+}
 
 export default function ChartComponent() {
-  // Crear arrays para presión, temperatura y profundidad
-  const pressureData = data.map(item => ({ value: item.Pressure, dataPointText: `Profundidad: ${item.Depth}` }));
-  const temperatureData = data.map(item => ({ value: item.Temperature, dataPointText: `Profundidad: ${item.Depth}` }));
-
-    // Tipos de fluido por parada
-    const fluidTypes = data.map(item => {
+  const pressureData = data.map(item => ({ 
+    value: item.Pressure, 
+    label: item.Time,
+    depth: item.Depth,
+    dataPointText: `Profundidad: ${item.Depth}` 
+  }));
+  const temperatureData = data.map(item => ({ 
+    value: item.Temperature, 
+    depth: item.Depth,
+    temp: item.Temperature,
+    dataPointText: `Profundidad: ${item.Depth}`
+  }));
+  const fluidTypes = data.map(item => {
     const density = calculateDensity(item.Pressure, item.Depth);
     let fluidType = "Desconocido";
     if (density >= 800 && density <= 900) {
@@ -27,11 +34,11 @@ export default function ChartComponent() {
         fluidType = "Gas";
     }
     return fluidType;
-    });
-
+  });
+   
   return (
     <ScrollView>
-        <View style = {{marginTop: 30}}></View>
+      <View style={{marginTop: 30}}></View>
       <Text>Gráfico de Presión vs Profundidad</Text>
       <LineChart
         initialSpacing={20}
@@ -39,7 +46,7 @@ export default function ChartComponent() {
         height={250}
         maxValue={5800}
         showVerticalLines
-        spacing={94}
+        spacing={23}
         color1="orange"
         textColor1="green"
         dataPointsHeight={6}
@@ -48,28 +55,89 @@ export default function ChartComponent() {
         textShiftY={-2}
         textShiftX={-5}
         textFontSize={11}
+        pointerConfig={{
+          pointerStripUptoDataPoint: true,
+          pointerStripColor: 'lightgray',
+          pointerStripWidth: 2,
+          strokeDashArray: [2, 5],
+          pointerColor: 'lightgray',
+          radius: 4,
+          pointerLabelWidth: 100,
+          pointerLabelHeight: 120,
+          pointerLabelComponent: items => {
+            const item = items[0];
+            console.log({item})
+            return (
+              <View
+                style={{
+                  height: 120,
+                  width: 100,
+                  backgroundColor: '#282C3E',
+                  borderRadius: 4,
+                  justifyContent:'center',
+                  paddingLeft:16,
+                }}>
+                <Text style={{color: 'lightgray',fontSize:12}}>Presión</Text>
+                <Text style={{color: 'white', fontWeight:'bold'}}>{item.value}</Text>
+                <Text style={{color: 'lightgray',fontSize:12,marginTop:12}}>Profundidad</Text>
+                <Text style={{color: 'white', fontWeight:'bold'}}>{item.depth}</Text>
+              </View>
+            );
+          },
+        }}
+        
       />
       <Text>Gráfico de Temperatura vs Profundidad</Text>
       <LineChart
         initialSpacing={20}
         data={temperatureData}
         height={350}
-        maxValue={70}
+        maxValue={65}
         yAxisOffset={110}
         showVerticalLines
-        spacing={94}
+        spacing={23}
         color1="skyblue"
         textColor1="green"
         dataPointsHeight={6}
         dataPointsWidth={6}
         dataPointsColor1="red"
-        textShiftY={-10}
-        textShiftX={-5}
+        textShiftY={-6}
+        textShiftX={5}
         textFontSize={11}
+        pointerConfig={{
+          pointerStripUptoDataPoint: true,
+          pointerStripColor: 'lightgray',
+          pointerStripWidth: 2,
+          strokeDashArray: [2, 5],
+          pointerColor: 'lightgray',
+          radius: 4,
+          pointerLabelWidth: 100,
+          pointerLabelHeight: 120,
+          pointerLabelComponent: (items) => {
+            const item = items[0];
+            console.log(item)
+            return (
+              <View
+                style={{
+                  height: 130,
+                  width: 150,
+                  backgroundColor: '#282C3E',
+                  borderRadius: 4,
+                  justifyContent:'center',
+                  paddingLeft:16,
+                }}>
+                <Text style={{color: 'lightgray',fontSize:12}}>Temperatura</Text>
+                <Text style={{color: 'white', fontWeight:'bold'}}>{item.temp}</Text>
+                <Text style={{color: 'lightgray',fontSize:12,marginTop:12}}>Profundidad</Text>
+                <Text style={{color: 'white', fontWeight:'bold'}}>{item.depth}</Text>
+              </View>
+            );
+          },
+        }}
       />
       <Text>Tipos de Fluido por Parada:</Text>
       {fluidTypes.map((fluidType, index) => (
-        <Text key={index}>{`PARADA N° ${index + 1}: ${fluidType}`}</Text>
+          <Text key={index}>{`PARADA N° ${index + 1}: ${fluidType}`}</Text>
       ))}
     </ScrollView>
   );
